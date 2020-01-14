fcbd5f4f6e6350560998aae675d7d661294241c1
Alexandre Medina <ammedina@inf.ufsm.br>
2020-01-14 12:15:08 -0300

    accept null | saving as integer value

--- src/component.vue
+++ src/component.vue
@@ -1,78 +1,59 @@
 <template lang="html">
-  <input type="tel"
-         :value="formattedValue"
-         @change="change"
-         v-money="{precision, decimal, thousands, prefix, suffix, allowBlank}"
-         class="v-money" />
+    <input type="tel"
+           v-model="formattedValue"
+           v-money="{precision, decimal, thousands, prefix, suffix, allowBlank}"
+           class="v-money"/>
 </template>
 
 <script>
-import money from './directive'
-import defaults from './options'
-import {format, unformat} from './utils'
+    import money from './directive'
+    import defaults from './options'
+    import {onlyNumbers, unformat} from './utils'
 
-export default {
-  name: 'Money',
-  props: {
-    value: {
-      required: true,
-      type: [String],
-      default: null
-    },
-    masked: {
-      type: Boolean,
-      default: false
-    },
-    precision: {
-      type: Number,
-      default: () => defaults.precision
-    },
-    decimal: {
-      type: String,
-      default: () => defaults.decimal
-    },
-    thousands: {
-      type: String,
-      default: () => defaults.thousands
-    },
-    prefix: {
-      type: String,
-      default: () => defaults.prefix
-    },
-    suffix: {
-      type: String,
-      default: () => defaults.suffix
-    },
-    allowBlank: {
-      type: Boolean,
-      default: () => defaults.allowBlank
+    export default {
+        name: 'Money',
+        props: {
+            value: {
+                required: false,
+                default: null
+            },
+            precision: {
+                type: Number,
+                default: () => defaults.precision
+            },
+            decimal: {
+                type: String,
+                default: () => defaults.decimal
+            },
+            thousands: {
+                type: String,
+                default: () => defaults.thousands
+            },
+            prefix: {
+                type: String,
+                default: () => defaults.prefix
+            },
+            suffix: {
+                type: String,
+                default: () => defaults.suffix
+            },
+            allowBlank: {
+                type: Boolean,
+                default: () => defaults.allowBlank
+            }
+        },
+        directives: {money},
+        data() {
+            return {
+                formattedValue: this.$props.value,
+            }
+        },
+        watch: {
+            formattedValue(newValue, oldValue) {
+                if (oldValue !== newValue) {
+                    this.$emit('input', onlyNumbers(newValue))
+                }
+            }
+        },
     }
-  },
-
-  directives: {money},
-
-  data () {
-    return {
-      formattedValue: ''
-    }
-  },
-
-  watch: {
-    value: {
-      immediate: true,
-      handler (newValue, oldValue) {
-        var formatted = format(newValue, this.$props)
-        if (formatted !== this.formattedValue) {
-          this.formattedValue = formatted
-        }
-      }
-    }
-  },
-
-  methods: {
-    change (evt) {
-      this.$emit('input', this.masked ? evt.target.value : unformat(evt.target.value, this.precision))
-    }
-  }
-}
 </script>
--- src/directive.js
+++ src/directive.js
@@ -1,44 +1,46 @@
-import {format, unformat, setCursor, event} from './utils'
-import assign from './assign'
-import defaults from './options'
+import {
+    format, unformat, setCursor, event,
+} from './utils';
+import assign from './assign';
+import defaults from './options';
 
 export default function (el, binding) {
-  if (!binding.value) return
-  var opt = assign(defaults, binding.value)
+    if (!binding.value) return;
+    const opt = assign(defaults, binding.value);
 
-  // v-money used on a component that's not a input
-  if (el.tagName.toLocaleUpperCase() !== 'INPUT') {
-    var els = el.getElementsByTagName('input')
-    if (els.length !== 1) {
-      // throw new Error("v-money requires 1 input, found " + els.length)
-    } else {
-      el = els[0]
+    // v-money used on a component that's not a input
+    if (el.tagName.toLocaleUpperCase() !== 'INPUT') {
+        const els = el.getElementsByTagName('input');
+        if (els.length !== 1) {
+            // throw new Error("v-money requires 1 input, found " + els.length)
+        } else {
+            el = els[0];
+        }
     }
-  }
 
-  el.onkeydown = function (e) {
-    var backspacePressed = e.which == 8 || e.which == 46
-    var atEndPosition = (el.value.length - el.selectionEnd) === 0
-    if (opt.allowBlank && backspacePressed && atEndPosition && (unformat(el.value, 0) === 0)) {
-      el.value = ''
-      el.dispatchEvent(event('change')) // v-model.lazy
-    }
-  }
+    el.onkeydown = function (e) {
+        const backspacePressed = e.which == 8 || e.which == 46;
+        const atEndPosition = (el.value.length - el.selectionEnd) === 0;
+        if (opt.allowBlank && backspacePressed && atEndPosition && (unformat(el.value, 0) === 0)) {
+            el.value = '';
+            el.dispatchEvent(event('change')); // v-model.lazy
+        }
+    };
 
-  el.oninput = function () {
-    var positionFromEnd = el.value.length - el.selectionEnd
-    el.value = format(el.value, opt)
-    positionFromEnd = Math.max(positionFromEnd, opt.suffix.length) // right
-    positionFromEnd = el.value.length - positionFromEnd
-    positionFromEnd = Math.max(positionFromEnd, opt.prefix.length + 1) // left
-    setCursor(el, positionFromEnd)
-    el.dispatchEvent(event('change')) // v-model.lazy
-  }
+    el.oninput = function () {
+        let positionFromEnd = el.value.length - el.selectionEnd;
 
-  el.onfocus = function () {
-    setCursor(el, el.value.length - opt.suffix.length)
-  }
+        el.value = format(el.value, opt);
+        positionFromEnd = Math.max(positionFromEnd, opt.suffix.length); // right
+        positionFromEnd = el.value.length - positionFromEnd;
+        positionFromEnd = Math.max(positionFromEnd, opt.prefix.length + 1); // left
+        setCursor(el, positionFromEnd);
+        el.dispatchEvent(event('change')); // v-model.lazy
+    };
 
-  el.oninput()
-  el.dispatchEvent(event('input')) // force format after initialization
+    el.onfocus = function () {
+        setCursor(el, el.value.length - opt.suffix.length);
+    };
+
+    el.oninput();
 }
